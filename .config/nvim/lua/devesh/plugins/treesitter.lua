aa615f887b80a6ee85eed30f3b5c3eaf54f307bf
Devesh Mhatre <deveshmhtr.mhatre92@gmail.com>
2025-11-16 10:59:25 +0530

    Use main branch of treesitter

--- .config/nvim/lua/devesh/plugins/treesitter.lua
+++ .config/nvim/lua/devesh/plugins/treesitter.lua
@@ -1,61 +1,17 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	event = { "BufReadPre", "BufNewFile" },
-
+	lazy = false,
+	branch = "main",
 	build = ":TSUpdate",
-	dependencies = {
-		"windwp/nvim-ts-autotag",
-		"RRethy/nvim-treesitter-endwise",
-	},
 	config = function()
-		-- import nvim-treesitter-plugin
-		local treesitter = require("nvim-treesitter.configs")
+		local treesitter = require("nvim-treesitter")
 
-		-- configure treesitter
+		treesitter.install({ "javascript", "lua", "ruby", "rust", "typescript" })
+
 		treesitter.setup({
-			highlight = {
-				enable = true,
-			},
-
-			-- enable endwise for ruby
-			endwise = { enable = { "ruby" } },
-
-			-- enable indentation
-			indent = { enable = true },
-
-			-- enable autotagging (w/ nvim-ts-autotag plugin)
-			autotag = {
-				enable = true,
-			},
-
-			-- ensure these language parsers are installed
-			ensure_installed = {
-				"json",
-				"javascript",
-				"typescript",
-				"tsx",
-				"yaml",
-				"html",
-				"css",
-				"markdown",
-				"bash",
-				"lua",
-				"vim",
-				"dockerfile",
-				"gitignore",
-				"vimdoc",
-				"c",
-				"cpp",
-				"ruby",
-				"svelte",
-			},
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<C-space>",
-					node_incremental = "<C-space>",
-					scope_incremental = false,
-					node_decremental = "<bs>",
+			defaults = {
+				preview = {
+					treesitter = false,
 				},
 			},
 		})
